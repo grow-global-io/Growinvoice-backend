f344fa9c7e5fb1bce60d8df48f0e069c0ec9841f
somashekar <93281123+somashekar4138@users.noreply.github.com>
2024-06-11 03:06:58 +0530

    refactor: Remove commented code and unused middleware logic
    The code changes in this commit remove commented code and unused middleware logic in the `prisma/middleware.ts` file. This cleanup improves the readability and maintainability of the codebase by removing unnecessary code and reducing complexity.

--- src/prisma/middleware.ts
+++ src/prisma/middleware.ts
@@ -11,45 +11,24 @@ export function SoftDeleteMiddleware<T>(): Prisma.Middleware {
       // Ignore queries for models in the white list
       return next(params);
     }
-    if (params.action === 'findMany') {
-      // Find unique queries
-      // Add a condition to the query
-      params.args['where'] = {
-        ...params.args['where'],
-        isExist: true,
-      };
-    }
-    if (params.action === 'findFirst') {
-      // Find unique queries
-      // Add a condition to the query
-      params.args['where'] = {
-        ...params.args['where'],
-        isExist: true,
-      };
-    }
-    if (params.action === 'findUnique') {
-      // Find unique queries
-      // Add a condition to the query
-      params.args['where'] = {
-        ...params.args['where'],
-        isExist: true,
-      };
-    }
-    if (params.action === 'delete') {
-      // Delete queries
-      // Change action to an update
-      params.action = 'update';
-      params.args['data'] = {
-        isExist: false,
-      };
-    }
-    if (params.action === 'deleteMany') {
-      // Delete many queries
-      params.action = 'updateMany';
-      params.args['data'] = {
-        isExist: false,
-      };
-    }
+
+    // if (params.action === 'findFirst') {
+    //   // Find unique queries
+    //   // Add a condition to the query
+    //   params.args['where'] = {
+    //     ...params.args['where'],
+    //     isExist: true,
+    //   };
+    // }
+    // if (params.action === 'findUnique') {
+    //   // Find unique queries
+    //   // Add a condition to the query
+    //   params.args['where'] = {
+    //     ...params.args['where'],
+    //     isExist: true,
+    //   };
+    // }
+
     return next(params);
   };
 }
