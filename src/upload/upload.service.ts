089db4eacd202521c16099d3afe98790cb7957dd
somashekar <93281123+somashekar4138@users.noreply.github.com>
2024-07-05 02:31:31 +0530

    chore: Update npm dependencies and add zlib for file compression

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -4,6 +4,8 @@ import { ConfigService } from '@nestjs/config';
 import { plainToInstance } from 'class-transformer';
 import { uuid } from 'uuidv4';
 import { UploadResponseDto } from './dto/upload-response.dto';
+import * as zlib from 'zlib';
+import { promisify } from 'util';
 
 @Injectable()
 export class UploadService {
@@ -12,6 +14,8 @@ export class UploadService {
   readonly azureConnectionString = this.configService.get<string>(
     'AZURE_STORAGE_CONNECTION_STRING',
   );
+
+  gunzip = promisify(zlib.gunzip);
 
   async upload(file: Express.Multer.File): Promise<UploadResponseDto> {
     const blobServiceClient = BlobServiceClient.fromConnectionString(
@@ -23,7 +27,19 @@ export class UploadService {
     const blockBlobClient = containerClient.getBlockBlobClient(
       uuid() + file.originalname,
     );
-    await blockBlobClient.uploadData(file.buffer, {
+
+    let fileBuffer = file.buffer;
+    if (
+      file.mimetype === 'application/gzip' ||
+      file.originalname.endsWith('.gz')
+    ) {
+      try {
+        fileBuffer = await this.gunzip(file.buffer);
+      } catch (error) {
+        throw new Error('Failed to decompress gzip file');
+      }
+    }
+    await blockBlobClient.uploadData(fileBuffer, {
       blobHTTPHeaders: { blobContentType: file.mimetype },
     });
     return plainToInstance(UploadResponseDto, {
