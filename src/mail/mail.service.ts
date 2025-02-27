503b9236cccc841613925e0a81abc83fbcb3ff9b
somashekar <20k65a0241@sasi.ac.in>
2025-02-27 16:55:23 +0530

    fix: correct extra space in commented-out password reset email subject line

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -48,7 +48,7 @@ export class MailService {
     );
     // await this.transporter.sendMail({
     //   to: email,
-    //   subject: 'Password Reset',
+    //   subject: 'Password  Reset',
     //   html: `Click <a href="${url}">here</a> to reset your password`,
     // });
   }
