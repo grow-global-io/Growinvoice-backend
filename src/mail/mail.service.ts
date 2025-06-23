1274279075b160c9c6b6ba67f97752176f0de944
somashekar <somasekhar.puralasetti@ateliasoftware.com>
2025-06-23 12:05:04 +0530

    feat: add nodemailer types and update MailService to use nodemailer for email sending

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,61 +1,65 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-// import * as nodemailer from 'nodemailer';
+import * as nodemailer from 'nodemailer';
 import { SendMailDto } from './dto/send-mail.dto';
 import { User } from '@shared/decorators/user.decorator';
 import { PrismaService } from '@/prisma/prisma.service';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 @Injectable()
 export class MailService {
-  // private transporter;
+  private transporter: nodemailer.Transporter<
+    SMTPTransport.SentMessageInfo,
+    SMTPTransport.Options
+  >;
 
   constructor(
     private configService: ConfigService,
     private readonly prismaService: PrismaService,
   ) {
-    // this.transporter = nodemailer.createTransport({
-    //   host: this.configService.get<string>('SMTP_HOST'),
-    //   port: this.configService.get<number>('SMTP_PORT'),
-    //   auth: {
-    //     user: this.configService.get<string>('SMTP_USER'),
-    //     pass: this.configService.get<string>('SMTP_PASS'),
-    //   },
-    // });
+    this.transporter = nodemailer.createTransport({
+      host: this.configService.get<string>('SMTP_HOST'),
+      port: this.configService.get<number>('SMTP_PORT'),
+      auth: {
+        user: this.configService.get<string>('SMTP_USER'),
+        pass: this.configService.get<string>('SMTP_PASS'),
+      },
+    });
   }
 
   async sendEmail(email: string, token: string) {
     const url = `${this.configService.get<string>('FRONTEND_URL')}/reset-password?token=${token}`;
-    const options = {
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        'content-type': 'application/json',
-        'api-key': this.configService.get<string>('BREVO_API_KEY'),
-      },
-      body: JSON.stringify({
-        sender: {
-          name: 'Grow Global Strategies Pvt Ltd',
-          email: 'operations@growglobal.io',
-        },
-        to: [
-          {
-            email: email,
-            name: email,
-          },
-        ],
-        subject: 'Password Reset',
-        htmlContent: `Click <a href="${url}">here</a> to reset your password`,
-      }),
-    };
-    const response = await fetch(
-      'https://api.brevo.com/v3/smtp/email',
-      options,
-    );
-    // await this.transporter.sendMail({
-    //   to: email,
-    //   subject: 'Password  Reset',
-    //   html: `Click <a href="${url}">here</a> to reset your password`,
-    // });
+    // const options = {
+    //   method: 'POST',
+    //   headers: {
+    //     accept: 'application/json',
+    //     'content-type': 'application/json',
+    //     'api-key': this.configService.get<string>('BREVO_API_KEY'),
+    //   },
+    //   body: JSON.stringify({
+    //     sender: {
+    //       name: 'Grow Global Strategies Pvt Ltd',
+    //       email: 'operations@growglobal.io',
+    //     },
+    //     to: [
+    //       {
+    //         email: email,
+    //         name: email,
+    //       },
+    //     ],
+    //     subject: 'Password Reset',
+    //     htmlContent: `Click <a href="${url}">here</a> to reset your password`,
+    //   }),
+    // };
+    // const response = await fetch(
+    //   'https://api.brevo.com/v3/smtp/email',
+    //   options,
+    // );
+    await this.transporter.sendMail({
+      to: email,
+      subject: 'Password  Reset',
+      html: `Click <a href="${url}">here</a> to reset your password`,
+    });
   }
 
   async sendMail(
@@ -78,38 +82,42 @@ export class MailService {
     if (companyName) {
       companyNameString = companyName;
     }
-    const options = {
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        'content-type': 'application/json',
-        'api-key': this.configService.get<string>('BREVO_API_KEY'),
+    // const options = {
+    //   method: 'POST',
+    //   headers: {
+    //     accept: 'application/json',
+    //     'content-type': 'application/json',
+    //     'api-key': this.configService.get<string>('BREVO_API_KEY'),
+    //   },
+    //   body: JSON.stringify({
+    //     sender: {
+    //       name: companyNameString ?? 'Grow Global Strategies Pvt Ltd',
+    //       email: 'operations@growglobal.io',
+    //     },
+    //     to: [
+    //       {
+    //         email: sendMailDto?.email,
+    //         name: sendMailDto?.email,
+    //       },
+    //     ],
+    //     subject: sendMailDto.subject,
+    //     htmlContent: sendMailDto.body,
+    //   }),
+    // };
+
+    // const response = await fetch(
+    //   'https://api.brevo.com/v3/smtp/email',
+    //   options,
+    // );
+
+    await this.transporter.sendMail({
+      to: sendMailDto.email,
+      subject: sendMailDto.subject,
+      html: sendMailDto.body,
+      sender: {
+        name: companyNameString ?? 'Grow Global Strategies Pvt Ltd',
+        address: 'no-reply@growinvoice.com',
       },
-      body: JSON.stringify({
-        sender: {
-          name: companyNameString ?? 'Grow Global Strategies Pvt Ltd',
-          email: 'operations@growglobal.io',
-        },
-        to: [
-          {
-            email: sendMailDto?.email,
-            name: sendMailDto?.email,
-          },
-        ],
-        subject: sendMailDto.subject,
-        htmlContent: sendMailDto.body,
-      }),
-    };
-
-    const response = await fetch(
-      'https://api.brevo.com/v3/smtp/email',
-      options,
-    );
-
-    // await this.transporter.sendMail({
-    //   to: sendMailDto.email,
-    //   subject: sendMailDto.subject,
-    //   html: sendMailDto.body,
-    // });
+    });
   }
 }
