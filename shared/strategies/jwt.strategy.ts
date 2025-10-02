e3a5f1be298f55de8ea5d43863729873710f1173
somashekar <somasekhar.puralasetti@ateliasoftware.com>
2025-10-02 22:36:45 +0530

    feat: Enhance JwtStrategy to assign user ID to payload if missing

--- shared/strategies/jwt.strategy.ts
+++ shared/strategies/jwt.strategy.ts
@@ -3,10 +3,11 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { jwtConstants } from '../../src/auth/constants';
 import { TokenUserDto } from '../../src/auth/dto/token-user.dto';
+import { PrismaService } from '@/prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly prismaService: PrismaService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -15,6 +16,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: TokenUserDto) {
+    if (!payload.sub) {
+      const user = await this.prismaService.user.findFirst({
+        where: {
+          email: payload.email,
+        },
+      });
+      payload.sub = user.id;
+    }
+    console.log('JWT Payload:', payload); // Debugging line
     return payload;
   }
 }
