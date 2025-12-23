5dabc313dc99e76c205ee8c25d22e03cff5d6cb9
Souvik Basu <bsouvik986.com>
2025-12-23 19:01:59 +0530

    feat: Add Shiprocket integration for shipment tracking and order creation

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -3,9 +3,10 @@ import { StoreService } from './store.service';
 import { StoreController } from './store.controller';
 import { MailService } from '@/mail/mail.service';
 import { ConfigService } from '@nestjs/config';
+import { ShiprocketModule } from '@/shiprocket/shiprocket.module';
 
 @Module({
-  imports: [],
+  imports: [ShiprocketModule],
   controllers: [StoreController],
   providers: [StoreService, MailService, ConfigService],
 })
